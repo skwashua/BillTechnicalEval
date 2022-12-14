393ba9f7adb5cc2d1ce8663d1afc337e92228f0d
Josh Lytle <josh@joshlytle.com>
2022-12-14 12:58:22 -0700

    Initial Commit

---
+++ BILLTechnicalExercise/BILLTechnicalExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BILLTechnicalExerciseApp.swift
+//  BILLTechnicalExercise
+//
+//  Created by Joshua Lytle on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BILLTechnicalExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
